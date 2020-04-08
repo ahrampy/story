927882ce12c3b3e1e036b9ea66c87fbbc323feff
adrian_rampy <ahrampy@gmail.com>
2020-04-08 16:00:57 -0700

    add docs /ignored

--- src/components/TipTap.vue
+++ src/components/TipTap.vue
@@ -1,71 +1,42 @@
 <template>
-  <div>
+  <div id="editor">
     <editor-menu-bar :editor="editor" v-slot="{ commands, isActive }">
-      <button :class="{ 'is-active': isActive.bold() }" @click="commands.bold">Bold</button>
+      <button :class="{ 'is-active': isActive.bold() }" @click="commands.bold">
+        Bold
+      </button>
     </editor-menu-bar>
-    <editor-content :editor="editor" autofocus="true" />
+    <editor-content :editor="editor" />
   </div>
 </template>
 
 <script>
 import { Editor, EditorContent, EditorMenuBar } from "tiptap";
-import {
-  Blockquote,
-  CodeBlock,
-  HardBreak,
-  Heading,
-  OrderedList,
-  BulletList,
-  ListItem,
-  TodoItem,
-  TodoList,
-  Bold,
-  Code,
-  Italic,
-  Link,
-  Strike,
-  Underline,
-  History
-} from "tiptap-extensions";
+import { Bold, Italic, Strike, Underline } from "tiptap-extensions";
 
 export default {
   components: {
     EditorMenuBar,
-    EditorContent
+    EditorContent,
   },
   data() {
     return {
-      editor: null
+      editor: new Editor({
+        extensions: [new Bold(), new Italic(), new Strike(), new Underline()],
+        content: `
+          <h1>Yay Headlines!</h1>
+          <p>All these <strong>cool tags</strong> are working now.</p>
+        `,
+      }),
     };
-  },
-  mounted() {
-    this.editor = new Editor({
-      content: "editor text",
-      extensions: [
-        new Blockquote(),
-        new CodeBlock(),
-        new HardBreak(),
-        new Heading({ levels: [1, 2, 3] }),
-        new BulletList(),
-        new OrderedList(),
-        new ListItem(),
-        new TodoItem(),
-        new TodoList(),
-        new Bold(),
-        new Code(),
-        new Italic(),
-        new Link(),
-        new Strike(),
-        new Underline(),
-        new History()
-      ]
-    });
   },
   beforeDestroy() {
     this.editor.destroy();
-  }
+  },
 };
 </script>
 
 <style scoped>
+  #editor {
+    
+  }
 </style>
